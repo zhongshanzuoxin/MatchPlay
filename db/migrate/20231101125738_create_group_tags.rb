e6ceac4d5670313a55514b456e080396e6f420b7
EC2 Default User <ec2-user@ip-172-31-12-54.ap-northeast-1.compute.internal>
2023-11-21 10:56:46 +0000

    修正

--- db/migrate/20231101125738_create_group_tags.rb
+++ db/migrate/20231101125738_create_group_tags.rb
@@ -1,12 +1,12 @@
 class CreateGroupTags < ActiveRecord::Migration[6.1]
   def change
     create_table :group_tags do |t|
-      t.references :group, type: :integer, foreign_key: true,
+      t.references :group, type: :integer, foreign_key: true
       t.references :tag, type: :bigint, foreign_key: true
 
       t.timestamps
     end
-    
+
     add_index :group_tags, [:group_id, :tag_id], unique: true
   end
 end
