74c4dd9ca1b8de26699e31fad4121243d9d4fa95
EC2 Default User <ec2-user@ip-172-31-12-54.ap-northeast-1.compute.internal>
2023-12-05 12:27:04 +0000

    S3に関する設定

--- app/models/profile_icon.rb
+++ app/models/profile_icon.rb
@@ -1,10 +1,29 @@
 class ProfileIcon < ApplicationRecord
-  has_one_attached :image
-  
-    # S3バケット内の画像URLを返すメソッド
-  def image_url
+  has_one_attached :image, service: :amazon_original
+
+  # リサイズされた画像のURLを生成するメソッド
+  def resized_image_url
     if image.attached?
-      image.service_url
+      "https://match-play-bucket.s3.amazonaws.com/#{resized_image_key}"
     end
+  end
+
+  # リサイズされた画像のS3キーを生成するメソッド
+  def resized_image_key
+    "#{image.key.split('.')[0]}-thumbnail.#{image.filename.extension}" if image.attached?
+  end
+
+  # プロファイルアイコンが破棄された時にリサイズされた画像も削除する
+  after_destroy :delete_resized_image
+
+  private
+
+  def delete_resized_image
+    return unless image.attached?
+
+    s3_client = Aws::S3::Client.new(region: ENV['AWS_REGION'])
+    s3_client.delete_object(bucket: 'match-play-bucket', key: resized_image_key)
+  rescue Aws::S3::Errors::ServiceError => e
+    Rails.logger.error "Failed to delete resized image: #{e.message}"
   end
 end
